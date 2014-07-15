523cc81fcbbc6fc6951aa005f4375bec4197a05d
adrivanrex <adrivanrex@gmail.com>
2014-07-16 04:36:04 +0800

    beautified php

--- api/addDomain.php
+++ api/addDomain.php
@@ -5,9 +5,18 @@ $xampp = new XamppDomainAdder;
 header('Content-Type: application/json');
 $ip = $_GET['ip'];
 $domainName = $_GET['domain'];
-$addHost = $xampp->addHost($hostfileLocation,$vhostFileLocation,$ip,$domainName,$DocumentRoot,$domainName);
-$array = array(
-		    "output" => "Success"
-		);
-		echo json_encode($array);
+if($ip){
+	$addHost = $xampp->addHost($hostfileLocation,$vhostFileLocation,$ip,$domainName,$DocumentRoot,$domainName);
+	$array = array(
+			    "output" => "Success"
+			);
+			echo json_encode($array);
+		}
+else{
+	$array = array(
+			    "output" => "Failed"
+			);
+			echo json_encode($array);
+		}
+
 ?>
--- class/xampp.php
+++ class/xampp.php
@@ -1,62 +1,65 @@
 <?php
 include('config.php');
-class XamppDomainAdder {
-
-
-    function scrapeHost($fileLocation,$from){
+class XamppDomainAdder
+{
+    
+    
+    function scrapeHost($fileLocation, $from)
+    {
         $getContentFile = file_get_contents($fileLocation);
-        $arr = explode("\n", $getContentFile);
-        $arrCount = count($arr);
-
-        for ($x = 0;$x <= $from; $x++ ){
+        $arr            = explode("\n", $getContentFile);
+        $arrCount       = count($arr);
+        
+        for ($x = 0; $x <= $from; $x++) {
             unset($arr[$x]);
         }
         return $arr;
     }
-
-
-
-    function addHost($hostfileLocation,$vhostFileLocation,$ip,$domainName,$DocumentRoot,$ServerName){
+    
+    
+    
+    function addHost($hostfileLocation, $vhostFileLocation, $ip, $domainName, $DocumentRoot, $ServerName)
+    {
         
         $hostFileCurrent = file_get_contents($hostfileLocation);
         // Append a new person to the file
-        $hostFileCurrent .= "\r\n".$ip." ".$domainName."\n";
+        $hostFileCurrent .= "\r\n" . $ip . " " . $domainName . "\n";
         // Write the contents back to the file
         file_put_contents($hostfileLocation, $hostFileCurrent);
-
-    $vhostFileCurrent = file_get_contents($vhostFileLocation);
-    // Append a new person to the file
-    //$vhostFileCurrent .= "\r\n".$ip." ".$domainName."\n";
-    // Write the contents back to the file
-
-    $file = $vhostFileLocation;
-    // Open the file to get existing content
-    $current = file_get_contents($file);
-    // Append a new person to the file
-    $endline = "\n";
-    $current .= ''.$endline.'<VirtualHost *:80>
+        
+        $vhostFileCurrent = file_get_contents($vhostFileLocation);
+        // Append a new person to the file
+        //$vhostFileCurrent .= "\r\n".$ip." ".$domainName."\n";
+        // Write the contents back to the file
+        
+        $file    = $vhostFileLocation;
+        // Open the file to get existing content
+        $current = file_get_contents($file);
+        // Append a new person to the file
+        $endline = "\n";
+        $current .= '' . $endline . '<VirtualHost *:80>
         ServerAdmin webmaster@dummy-host2.example.com
-        DocumentRoot "'.$DocumentRoot.'/'.$ServerName.'"
-        ServerName '.$ServerName.'
+        DocumentRoot "' . $DocumentRoot . '/' . $ServerName . '"
+        ServerName ' . $ServerName . '
         ##ErrorLog "logs/dummy-host2.example.com-error.log"
         ##CustomLog "logs/dummy-host2.example.com-access.log" common
     </VirtualHost>';
-    // Write the contents back to the file
-    file_put_contents($file, $current);
-     $backslash = '\\';
-    $cmd = 'mkdir '.$DocumentRoot.''.$backslash.''.$ServerName.'';
-    $output = shell_exec(''.$cmd.'');
-
-     $myfile = fopen("restartApache.bat", "w") or die("Unable to open file!");
-      $txt = 'start /d "C:\xampp\apache\bin" httpd.exe -k restart';
-      fwrite($myfile, $txt);
-      fclose($myfile);
-    $output = shell_exec('restartApache.bat');
-
-
-  }
-
-
+        // Write the contents back to the file
+        file_put_contents($file, $current);
+        $backslash = '\\';
+        $cmd       = 'mkdir ' . $DocumentRoot . '' . $backslash . '' . $ServerName . '';
+        $output    = shell_exec('' . $cmd . '');
+        
+        $myfile = fopen("restartApache.bat", "w") or die("Unable to open file!");
+        $txt = 'start /d "C:\xampp\apache\bin" httpd.exe -k restart';
+        fwrite($myfile, $txt);
+        fclose($myfile);
+        $output = shell_exec('restartApache.bat');
+        
+        
+    }
+    
+    
 }
 
 
